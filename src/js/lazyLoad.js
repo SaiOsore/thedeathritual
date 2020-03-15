ce511963e26968f76aff871370afe6c24d0b627d
Sai Osore <sai.osore@gmail.com>
2020-03-15 17:36:36 +0200

    added lazyLoad for images with blur css effect

---
+++ src/js/lazyLoad.js
@@ -0,0 +1,20 @@
+let images = [...document.querySelectorAll('.lazy')]
+
+const interactSettings = {
+  //root: document.querySelector('.gallery__content'),
+  // rootMargin: '0px 0px 200px 0px'
+}
+
+function onIntersection(imageEntites) {
+  imageEntites.forEach(image => {
+    if (image.isIntersecting) {
+      observer.unobserve(image.target)
+      image.target.src = image.target.dataset.src
+      image.target.onload = () => image.target.classList.add('no-blur')
+    }
+  })
+}
+
+let observer = new IntersectionObserver(onIntersection, interactSettings)
+
+images.forEach(image => observer.observe(image))
