be35fcc8c27c1817d8ba4983a9d8e529dcf0d9e8
Sai Osore <sai.osore@gmail.com>
2020-03-23 01:18:43 +0200

    menu open bug fix, preloader/body bug fix

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -4,5 +4,5 @@ var mobileMenu = document.querySelector('.header__mobile-menu');
 wrapperMenu.addEventListener('click', function(){
   wrapperMenu.classList.toggle('header-mobile-btn--active');
   mobileMenu.classList.toggle('header__mobile-menu--active');
-  document.body.classList.toggle('body-fixed');
+  document.body.classList.toggle('overflow-hidden');
 })
